43d550db2771b7300f5d198a456bf657dfa986a4
kindakapil <geovelomega@gmail.com>
2023-09-23 22:58:03 +0530

    scrapy commented in streamlit pyython file

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import scrapy
-from scrapy.crawler import CrawlerProcess
+# import scrapy
+# from scrapy.crawler import CrawlerProcess
 
 
 # Import your QnA and Sentiment Analysis programs
